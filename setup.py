cd32893b01adbcb61fd2f6a3961fbaf58b1d4ff7
mustmiekieke <tommydeboer4@gmail.com>
2018-08-30 13:03:25 +0200

    add the MOLGENIS Python client

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name = 'mdev',
     version = '0.1.0',
-    packages = ['mdev'],
+    packages = ['mdev', 'client'],
     entry_points = {
         'console_scripts': [
             'mdev = mdev.__main__:main'
         ]
-    }
+    }, install_requires=['requests']
 )
