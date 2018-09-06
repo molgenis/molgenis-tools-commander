9c1d3c29a6a51a5505920923587427982581fd01
mustmiekieke <tommydeboer4@gmail.com>
2018-09-06 08:58:53 +0200

    partial implementation of import command and refactoring of request code

--- mdev/commands.py
+++ mdev/commands.py
@@ -1,4 +1,6 @@
 import json
+from os import path, listdir, getcwd
+from os.path import splitext, join
 
 import requests
 
@@ -12,7 +14,40 @@ token = ''
 
 
 def import_(args):
-    log.info('Importing %s', args.file)
+    if path.isfile(args.file):
+        # TODO importing from an absolute path
+        log.warn('Importing from an absolute path')
+        exit(1)
+    elif path.isfile(join(getcwd(), args.file)):
+        # TODO importing from current directory
+        log.warn('Importing from the current directory')
+        exit(1)
+    else:
+        # importing from quick-folders
+        if not config.has_option('datasets', 'git_root'):
+            log.warn('Molgenis git root not set. Edit the mdev.ini file to include the test datasets.')
+
+        paths_string = ''.join(config.get('datasets', 'git_paths').split())
+        paths = paths_string.split(',')
+        map(str.strip, paths)
+
+        files = dict()
+        for folder in paths:
+            if not path.isdir(folder):
+                log.warn('Folder %s is not a valid folder, skipping it...', folder)
+
+            for file in listdir(folder):
+                file_name = splitext(file)[0]
+                files[file_name] = join(folder, file)
+
+        print(files)
+
+        if splitext(args.file)[0] in files:
+            print(files[splitext(args.file)[0]])
+
+
+def _import_file(file_path):
+    pass
 
 
 def make(args):
@@ -27,12 +62,10 @@ def make(args):
 def _find_group(role):
     log.debug('Fetching groups')
     groups = _get(config.get('api', 'rest2') + 'sys_sec_Group?attrs=name')
+    role = lower_kebab(role)
 
-    matches = dict()
-    for group in groups.json()['items']:
-        group_name = group['name']
-        if lower_kebab(role).startswith(group_name):
-            matches[len(group_name)] = group_name
+    matches = {len(group['name']): group['name'] for group in groups.json()['items'] if role.startswith(group['name'])}
+
     if not matches:
         log.error('No group found for role %s', role.upper())
         exit(1)
@@ -53,6 +86,7 @@ def add(args):
 
 def _add_user(username):
     log.info('Adding user %s', username)
+
     _post(config.get('api', 'rest1') + 'sys_sec_User',
           {'username': username,
            'password_': username,
@@ -77,42 +111,31 @@ def _login():
     password = config.get('auth', 'password')
 
     log.debug('Logging in as user %s', username)
-    try:
-        response = requests.post(login_url,
-                                 data=json.dumps({"username": username, "password": password}),
-                                 headers={"Content-Type": "application/json"})
-        response.raise_for_status()
-        token = response.json()['token']
-    except requests.RequestException as e:
-        log.error(e)
-        exit(1)
+
+    response = _post(login_url,
+                     data={"username": username, "password": password})
+    token = response.json()['token']
 
 
 def _get(url):
-    response = str()
-    try:
-        response = requests.get(url,
-                                headers={'Content-Type': 'application/json',
-                                         'x-molgenis-token': token})
-        response.raise_for_status()
-        return response
-    except requests.HTTPError:
-        for error in response.json()['errors']:
-            log.error(error['message'])
-        exit(1)
-    except requests.RequestException as e:
-        log.error(e)
-        exit(1)
+    return _handle_request(lambda: requests.get(url,
+                                                headers={'Content-Type': 'application/json',
+                                                         'x-molgenis-token': token}))
 
 
 def _post(url, data):
+    return _handle_request(lambda: requests.post(url,
+                                                 headers={'Content-Type': 'application/json',
+                                                          'x-molgenis-token': token},
+                                                 data=json.dumps(data)))
+
+
+def _handle_request(request):
     response = str()
     try:
-        response = requests.post(url,
-                                 headers={'Content-Type': 'application/json',
-                                          'x-molgenis-token': token},
-                                 data=json.dumps(data))
+        response = request()
         response.raise_for_status()
+        return response
     except requests.HTTPError as e:
         if 'application/json' in response.headers.get('Content-Type'):
             if 'errors' in response.json():
