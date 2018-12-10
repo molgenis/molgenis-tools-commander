51cd44c9b36baff10b18a8d8766bd19518ffb045
tommydeboer <tommydeboer4@gmail.com>
2018-12-10 14:18:43 +0100

    fix first time installer

--- mdev/config/config.py
+++ mdev/config/config.py
@@ -1,29 +1,27 @@
 import shutil
 from configparser import ConfigParser
-from os import path, makedirs
+from pathlib import Path
 
 from mdev import io
+from mdev.config.home import get_properties_file
 
-_INSTALL_DIR = path.dirname(__file__)
-_DEFAULT_CONFIG = path.join(_INSTALL_DIR, 'config/defaults.ini')
-_USER_CONFIG_DIR = path.join(path.expanduser('~'), '.mdev')
-_USER_CONFIG = path.join(_USER_CONFIG_DIR, 'mdev.ini')
+_INSTALL_DIR = Path(__file__)
+_DEFAULT_CONFIG = _INSTALL_DIR.parents[0].joinpath('default.properties')
 
 _config = None
 
 
 def _create_user_config():
     try:
-        makedirs(_USER_CONFIG_DIR, exist_ok=True)
-        shutil.copyfile(_DEFAULT_CONFIG, _USER_CONFIG)
+        shutil.copyfile(_DEFAULT_CONFIG, get_properties_file())
     except OSError as err:
         io.error("Error setting up user configuration: %s" % err.strerror)
         exit(1)
 
 
 def _check_user_config():
-    if not path.isfile(_USER_CONFIG):
-        io.info("No user configuration file found. Creating it now.")
+    if not get_properties_file().exists():
+        io.info("No properties file found. Creating it now.")
         _create_user_config()
 
 
@@ -31,7 +29,7 @@ def _load_config():
     global _config
     _check_user_config()
     _config = ConfigParser()
-    _config.read(_USER_CONFIG)
+    _config.read(get_properties_file())
 
 
 def config():
