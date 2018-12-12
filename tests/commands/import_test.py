797416040b60dcf636d7c2934ce7516161fee8d8
tommydeboer <tommydeboer4@gmail.com>
2018-12-12 12:26:17 +0100

    fix tests

--- tests/commands/import_test.py
+++ tests/commands/import_test.py
@@ -12,34 +12,16 @@ class ImportMethodsTest(unittest.TestCase):
         multi_choice.return_value = 'other_file.xlsx'
         a1 = Attachment('url/1234/file.xlsx')
         a2 = Attachment('url/4567/other_file.xlsx')
-        ret = import_._choose_attachments([a1, a2])
-        self.assertEqual(ret, [a2])
-
-    @patch('mdev.io.multi_choice')
-    def test_choose_attachments_all(self, multi_choice):
-        multi_choice.return_value = 'All'
-        a1 = Attachment('url/1234/file.xlsx')
-        a2 = Attachment('url/4567/other_file.xlsx')
-        a3 = Attachment('url/4567/file.xlsx')
-        ret = import_._choose_attachments([a1, a2, a3])
-        self.assertEqual(ret, [a1, a2, a3])
+        ret = import_._choose_attachment([a1, a2])
+        self.assertEqual(ret, a2)
 
     @patch('mdev.io.multi_choice')
     def test_choose_attachments_with_duplicates(self, multi_choice):
         multi_choice.return_value = '1234/file.xlsx'
         a1 = Attachment('url/1234/file.xlsx')
         a2 = Attachment('url/4567/file.xlsx')
-        ret = import_._choose_attachments([a1, a2])
-        self.assertEqual(ret, [a1])
-
-    @patch('mdev.io.multi_choice')
-    def test_choose_attachments_all_with_duplicates(self, multi_choice):
-        multi_choice.return_value = 'All'
-        a1 = Attachment('url/1234/file.xlsx')
-        a2 = Attachment('url/4567/file.xlsx')
-        a3 = Attachment('url/1234/other_file.xlsx')
-        ret = import_._choose_attachments([a1, a2, a3])
-        self.assertEqual(ret, [a1, a2, a3])
+        ret = import_._choose_attachment([a1, a2])
+        self.assertEqual(ret, a1)
 
     def test_create_attachment_map(self):
         a1 = Attachment('url/1234/file.xlsx')
