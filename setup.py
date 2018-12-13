2b27e08f85a82f9dd3a07f9ff1d6ebeed1e3c9d0
tommydeboer <tommydeboer4@gmail.com>
2018-12-13 14:19:03 +0100

    Specify versions of required packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='mdev',
-    version='0.0.6',
-    packages=['mdev', 'mdev.client', 'mdev.commands', 'mdev.config'],
-    description='The MOLGENIS command line development tool',
-    url='https://github.com/molgenis/molgenis-tools-mdev',
+    name='molgenis-commander',
+    version='0.0.9',
+    packages=['mcmd', 'mcmd.client', 'mcmd.commands', 'mcmd.config'],
+    description='A command line interface for Molgenis',
+    url='https://github.com/molgenis/molgenis-tools-commander',
     author='Tommy de Boer',
     author_email='tommydeboer4@gmail.com',
     license='GNU Lesser General Public License 3.0',
@@ -14,6 +14,6 @@ setup(
             'mdev = mdev.__main__:main'
         ]
     },
-    install_requires=['requests', 'rainbow_logging_handler', 'PyInquirer', 'halo', 'polling', 'PyGithub',
-                      'colorama==0.3.9'],
+    install_requires=['requests==2.21.0', 'rainbow_logging_handler==2.2.2', 'PyInquirer==1.0.3', 'halo==0.0.22',
+                      'polling==0.3.0', 'PyGithub==1.43.3', 'colorama==0.3.9']
 )
