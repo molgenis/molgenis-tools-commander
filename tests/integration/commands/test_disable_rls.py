0fd02760e0f1d9fa82e010b7c5f000dac5acd760
tommydeboer <tommydeboer4@gmail.com>
2019-02-13 11:17:20 +0100

    Add 'enable/disable rls' integration tests

---
+++ tests/integration/commands/test_disable_rls.py
@@ -0,0 +1,25 @@
+import pytest
+from mock import patch
+
+from tests.integration.permission_client import entity_is_row_level_secured
+from tests.integration.utils import run_commander
+
+
+@pytest.mark.integration
+@patch('mcmd.io.confirm')
+def test_disable_rls(are_you_sure, session, entity_type):
+    are_you_sure.return_value = True
+    run_commander('enable rls {}'.format(entity_type))
+    run_commander('disable rls {}'.format(entity_type))
+
+    assert not entity_is_row_level_secured(session, entity_type)
+
+
+@pytest.mark.integration
+@patch('mcmd.io.confirm')
+def test_disable_rls_cancel(are_you_sure, session, entity_type):
+    are_you_sure.return_value = False
+    run_commander('enable rls {}'.format(entity_type))
+    run_commander('disable rls {}'.format(entity_type))
+
+    assert entity_is_row_level_secured(session, entity_type)
