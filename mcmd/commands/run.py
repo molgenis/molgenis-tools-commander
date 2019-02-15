c26bb7aa45c9b6adef2d6c909f51e29d03b0db5b
tommydeboer <tommydeboer4@gmail.com>
2019-02-15 11:29:28 +0100

    Refactor script module + allow empty lines and comments

--- mcmd/commands/run.py
+++ mcmd/commands/run.py
@@ -3,10 +3,11 @@ from mcmd import io
 from mcmd.config.home import get_scripts_folder
 from mcmd.executor import execute
 
-
 # =========
 # Arguments
 # =========
+from mcmd.utils.utils import McmdError
+
 
 def arguments(subparsers):
     p_run = subparsers.add_parser('run',
@@ -26,23 +27,43 @@ def arguments(subparsers):
 
 def run(args):
     script = get_scripts_folder().joinpath(args.script)
+    lines = _read_script(script)
+    _run_script(not args.ignore_errors, lines)
 
-    lines = list()
+
+def _run_script(exit_on_error, lines):
+    for line in lines:
+        if _is_comment(line) or _is_empty(line):
+            continue
+
+        _run_command(exit_on_error, line)
+
+
+def _run_command(exit_on_error, line):
+    sub_args = arg_parser.parse_args(line.split(' '))
+    setattr(sub_args, 'arg_string', line)
+    if sub_args.command == 'run':
+        io.error("Can't use the run command in a script: %s" % line)
+        if exit_on_error:
+            exit(1)
+        else:
+            return
+
+    execute(sub_args, exit_on_error)
+
+
+def _read_script(script):
     try:
         with open(script) as file:
             lines = [line.rstrip('\n') for line in file]
     except OSError as e:
-        io.error('Error reading script: %s' % str(e))
+        raise McmdError('Error reading script: {}'.format(str(e)))
+    return lines
 
-    exit_on_error = not args.ignore_errors
-    for line in lines:
-        sub_args = arg_parser.parse_args(line.split(' '))
-        setattr(sub_args, 'arg_string', line)
-        if sub_args.command == 'run':
-            io.error("Can't use the run command in a script: %s" % line)
-            if exit_on_error:
-                exit(1)
-            else:
-                continue
 
-        execute(sub_args, exit_on_error)
+def _is_comment(line):
+    return line.strip().startswith('#')
+
+
+def _is_empty(line):
+    return line.isspace() or len(line) == 0
