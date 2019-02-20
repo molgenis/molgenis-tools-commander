c03b20aa8cd4b2cd0303c3db0472bb57f1780ba6
tommydeboer <tommydeboer4@gmail.com>
2019-02-20 15:43:36 +0100

    Move request handler to its own module

--- mcmd/client/molgenis_client.py
+++ mcmd/client/molgenis_client.py
@@ -5,6 +5,7 @@ from urllib.parse import urljoin
 import requests
 
 from mcmd.client import auth
+from mcmd.client.request_handler import request
 from mcmd.config import config
 from mcmd.logging import get_logger
 from mcmd.utils.utils import McmdError
@@ -35,46 +36,6 @@ class ResourceType(Enum):
 class PrincipalType(Enum):
     USER = 'user'
     ROLE = 'role'
-
-
-def request(func):
-    def handle_json_error(response_json):
-        if 'errors' in response_json:
-            for error in response_json['errors']:
-                raise McmdError(error['message'])
-        elif 'errorMessage' in response_json:
-            raise McmdError(response_json['errorMessage'])
-
-    def is_json(response):
-        return response.headers.get('Content-Type') and 'application/json' in response.headers.get('Content-Type')
-
-    def token_is_valid(response):
-        """There's no real way to figure out if a token is valid or not. The best we can do is assume that any
-        'no read metadata' error means that the user isn't logged in."""
-        if response.status_code == 401 and is_json(response):
-            error = response.json()['errors'][0]
-            if 'code' in error and error['code'] == 'DS04' and 'message' in error and error['message'].startswith(
-                    "No 'Read metadata' permission"):
-                return False
-        return True
-
-    def handle_request(*args, **kwargs):
-        response = str()
-        try:
-            response = func(*args, **kwargs)
-            response.raise_for_status()
-            return response
-        except requests.HTTPError as e:
-            if not token_is_valid(response):
-                auth.login()
-                handle_request(*args, **kwargs)
-            if is_json(response):
-                handle_json_error(response.json())
-            raise McmdError(str(e))
-        except requests.RequestException as e:
-            raise McmdError(str(e))
-
-    return handle_request
 
 
 @request
