02ddeb7e269af4721769136c89fb6f3fefe56385
tommydeboer <tommydeboer4@gmail.com>
2019-02-22 15:57:33 +0100

    fix missing package in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='molgenis-commander',
     version='1.0.1',
-    packages=['mcmd', 'mcmd.client', 'mcmd.commands', 'mcmd.config'],
+    packages=['mcmd', 'mcmd.client', 'mcmd.commands', 'mcmd.config', 'mcmd.utils'],
     description='A command line interface for Molgenis',
     url='https://github.com/molgenis/molgenis-tools-commander',
     author='Tommy de Boer',
