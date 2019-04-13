5e75407492e1d787a281a169de0e1c80a952d21d
tommydeboer <tommydeboer4@gmail.com>
2019-04-13 11:27:18 +0200

    Remove temp module

--- mcmd/commands/__init__.py
+++ mcmd/commands/__init__.py
@@ -8,9 +8,7 @@ This package intialization makes every command module available for a global imp
 import glob
 from os.path import dirname, basename, isfile
 
-from mcmd.commands import blaat
 from mcmd.commands._registry import get_argument_adders
-from mcmd.commands.past import blaat
 
 modules = glob.glob(dirname(__file__) + "/*.py")
 __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.startswith('_')]
