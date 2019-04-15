5cd31ad10429b4ceb0219a8a5f064e908eee42be
tommydeboer <tommydeboer4@gmail.com>
2019-04-15 13:56:45 +0200

    Add docstrings to compatibility module

--- mcmd/version/compatibility.py
+++ mcmd/version/compatibility.py
@@ -1,3 +1,7 @@
+"""
+This module houses the framework for ensuring compatibility over multiple MOLGENIS versions.
+"""
+
 from collections import defaultdict
 from distutils.version import StrictVersion
 from functools import wraps
@@ -6,27 +10,51 @@ from typing import List
 from mcmd.version import molgenis_version
 
 _registry = defaultdict(dict)
-
 MIN_VERSION = '7.0.0'
 
 
 def version(version_):
+    """
+    Version decorator. Switches implementations based on the version of MOLGENIS.
+
+    Usage:
+    >>> @version('7.2.0')
+    >>> def impl():
+    >>>     print('Implementation for 7.2 and up')
+    >>>
+    >>> @version('8.0.0')
+    >>> def impl():
+    >>>     print('Implementation for 8.0 and up')
+
+    Methods using this decorator should have the same name and be in a module with the same name.
+
+    MOLGENIS versions lower than MIN_VERSION aren't supported but the chosen implementation will default to the
+    closest match.
+    """
+
     def registrar(func):
+        """
+        Registers all functions with the @version decorator and replaces them with a general getter that will decide at
+        runtime which implementation to return.
+        """
+
         @wraps(func)
-        def getter():
-            def wrapper(*args, **kwargs):
+        def wrapper():
+            def getter(*args, **kwargs):
+                """Returns the needed implementation based on the MOLGENIS version."""
+
                 id_ = _get_func_id(func)
                 available_versions = list(_registry[id_].keys())
                 v = _get_closest_version(available_versions)
                 wanted_func = _registry[_get_func_id(func)][v]
                 return wanted_func(*args, **kwargs)
 
-            return wrapper
+            return getter
 
         func_id = _get_func_id(func)
         _registry[func_id][version_] = func
 
-        return getter()
+        return wrapper()
 
     return registrar
 
