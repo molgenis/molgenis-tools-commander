4afe029051491867285fc81fcde68fd4e8e4de6f
tommydeboer <tommydeboer4@gmail.com>
2019-08-30 08:38:59 +0200

    Introduce command types and group the help output based on those

--- mcmd/commands/_registry.py
+++ mcmd/commands/_registry.py
@@ -1,19 +1,26 @@
 """
 Each command's argument adder is registered here.
 """
+from collections import defaultdict
 
-_ARGUMENT_ADDER_REGISTRY = dict()
+from mcmd.core.command import CommandType
+
+_ARGUMENT_ADDER_REGISTRY = defaultdict(dict)
 
 
-def get_argument_adders():
-    return [func for name, func in sorted(_ARGUMENT_ADDER_REGISTRY.items())]
+def get_argument_adders(type_: CommandType):
+    return [func for name, func in sorted(_ARGUMENT_ADDER_REGISTRY[type_].items())]
 
 
-def arguments(command_name):
+def get_command_names(type_: CommandType):
+    return [name for name, func in sorted(_ARGUMENT_ADDER_REGISTRY[type_].items())]
+
+
+def arguments(command_name, type_=CommandType.STANDARD):
     """Command argument adder registration decorator."""
 
     def decorator(func):
-        _ARGUMENT_ADDER_REGISTRY[command_name] = func
+        _ARGUMENT_ADDER_REGISTRY[type_][command_name] = func
         return func
 
     return decorator
