d871c209e29778da183a153f81658070c16a3bd4
tommydeboer <tommydeboer4@gmail.com>
2019-09-06 13:28:00 +0200

    fix(io): fix #50 replace PyInquirer with questionary library

--- mcmd/io/ask.py
+++ mcmd/io/ask.py
@@ -1,87 +1,58 @@
-from mcmd.io.io import spinner
+import questionary
+
+from mcmd.io import io
 
 
 def multi_choice(message, choices):
-    if spinner:
-        spinner.stop_and_persist()
-
-    message = {
-        'type': 'rawlist',
-        'name': 'answer',
-        'message': message,
-        'choices': choices
-    }
-
-    return _handle_question(message)
+    question = questionary.select(message, choices=choices)
+    return _ask(question)
 
 
 def checkbox(message, choices):
-    if spinner:
-        spinner.stop_and_persist()
-
     checks = [{'name': choice, 'value': idx} for idx, choice in enumerate(choices)]
 
-    message = {
-        'type': 'checkbox',
-        'name': 'answer',
-        'message': message,
-        'choices': checks,
-        'validate': lambda answer: 'You must choose at least one option.' if len(answer) == 0 else True
-    }
+    question = questionary.checkbox(message, choices=checks)
 
-    answer_ids = _handle_question(message)
+    answer_ids = _ask(question, _validate_empty_list)
     return [choices[idx] for idx in answer_ids]
 
 
 def input_(message, required=False):
-    if spinner:
-        spinner.stop_and_persist()
-
-    message = {
-        'type': 'input',
-        'name': 'answer',
-        'message': message,
-    }
+    question = questionary.text(message)
 
     if required:
-        message['validate'] = lambda answer: "This field can't be empty." if len(answer) == 0 else True
-
-    return _handle_question(message)
+        return _ask(question, _validate_empty_input)
+    else:
+        return _ask(question)
 
 
 def password(message):
-    if spinner:
-        spinner.warn()
-
-    message = {
-        'type': 'password',
-        'name': 'answer',
-        'message': message,
-    }
-
-    return _handle_question(message)
+    return _ask(questionary.password(message))
 
 
 def confirm(message):
-    if spinner:
-        spinner.stop_and_persist()
-
-    message = {
-        'type': 'confirm',
-        'default': False,
-        'name': 'answer',
-        'message': message
-    }
-
-    return _handle_question(message)
+    question = questionary.confirm(message, default=False)
+    return _ask(question)
 
 
-def _handle_question(question):
-    answer = prompt([question])
-    if not answer:
-        # empty result means that PyInquirer caught an InterruptException
+def _ask(question, validation=None):
+    answer = question.ask()
+
+    if answer is None:
+        # None result means that PyInquirer caught an InterruptException
         exit(0)
-    else:
-        if spinner:
-            spinner.start()
-        return answer['answer']
+    elif validation:
+        result = validation(answer)
+        if result is not True:
+            io.error(result)
+            return _ask(question, validation)
+
+    return answer
+
+
+def _validate_empty_list(answer):
+    return 'You must choose at least one option.' if len(answer) == 0 else True
+
+
+def _validate_empty_input(answer):
+    return "This field can't be empty." if len(answer) == 0 else True
