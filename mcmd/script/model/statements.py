26eb0d8a1c153cca0930fcf7f4be6d48d07915c2
tommy <tommydeboer4@gmail.com>
2020-06-20 16:01:36 +0200

    Convert statement classes to @attrs

--- mcmd/script/model/statements.py
+++ mcmd/script/model/statements.py
@@ -1,15 +1,20 @@
-from abc import abstractmethod, ABC
+from abc import abstractmethod
+from typing import Optional
+
+import attr
 
 from mcmd.script.model.templates import Template
 from mcmd.script.model.types_ import InputType, ValueType
 
 
+@attr.s(frozen=True)
 class Statement:
     """A single or multi-lined statement in a MC script."""
     pass
 
 
-class Assignment(ABC):
+@attr.s(frozen=True)
+class Assignment:
     """
     A statement that assigns a value. Should expose the value's name through the name property.
 
@@ -17,12 +22,10 @@ class Assignment(ABC):
     $input bool is_assigned
     """
 
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        pass
+    name: str = attr.ib()
 
 
+@attr.s(frozen=True)
 class Templatable:
     """
     A statement that contains one or more Templates. Should expose all its Templates' variables through the variables
@@ -34,14 +37,8 @@ class Templatable:
     def variables(self) -> set:
         pass
 
-    def __key(self):
-        return (self.variables)
-    
-    def __hash__ (self):
-        hash(self.__key())
 
-
-
+@attr.s(frozen=True)
 class Value(Statement, Templatable, Assignment):
     """
     A value assignment. Supports text (in the form of a Template), booleans and None values.
@@ -51,57 +48,30 @@ class Value(Statement, Templatable, Assignment):
     $value empty
     """
 
-    def __init__(self, name: str, value=None):
-        self._name = name
-        self.__set_type(value)
-        self._value = value
+    type: ValueType = attr.ib()
+    value = attr.ib()
 
-    def __set_type(self, value):
+    @classmethod
+    def from_untyped_value(cls, name: str, value):
         if value is None:
-            self._type = None
+            type_ = None
         elif isinstance(value, bool):
-            self._type = ValueType.BOOL
+            type_ = ValueType.BOOL
         elif isinstance(value, Template):
-            self._type = ValueType.TEMPLATE
+            type_ = ValueType.TEMPLATE
         else:
             raise ValueError('invalid value type: {}'.format(type(value)))
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def value(self):
-        return self._value
-
-    @property
-    def type(self) -> ValueType:
-        return self._type
+        return cls(name, type_, value)
 
     @property
     def variables(self) -> set:
-        if self._type == ValueType.TEMPLATE:
-            return self._value.variables
+        if self.type == ValueType.TEMPLATE:
+            return self.value.variables
         else:
             return set()
-    
-    def __eq__ (self, other):
-        if isinstance (other, Value):
-            if self._name != other._name: return False
-            if self._value != other._value: return False
-            if self._type != other._type: return False
-            if self._value.variables != other._value.variables: return False
-            return True
-        else:
-            return False
 
-    def __key(self):
-        return (self._name, self._value, self._type)
 
-    def __hash__(self):
-        return hash(self.__key())
-    
-
+@attr.s(frozen=True)
 class Input(Statement, Templatable, Assignment):
     """
     An input assignment. Doesn't have a value because the inputs are requested from the user at runtime. Supports
@@ -111,51 +81,18 @@ class Input(Statement, Templatable, Assignment):
     $input text type : "text"
     """
 
-    def __init__(self, name: str, type_: InputType, message: Template = None):
-        self._name = name
-        self._type = type_
-        self._message = message
-        self.__set_variables()
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def type(self) -> InputType:
-        return self._type
-
-    @property
-    def message(self) -> Template:
-        return self._message
+    type: InputType = attr.ib()
+    message: Optional[Template] = attr.ib(default=None)
 
     @property
     def variables(self) -> set:
-        return self._variables
-
-    def __set_variables(self):
-        variables = set()
-        if self._message is not None:
-            variables |= self._message.variables
-        self._variables = variables
-    
-    def __eq__ (self, other): 
-        if isinstance (other, Input):
-            if self._name != other._name: return False
-            if self._type != other._type: return False
-            if self._message != other._message: return False
-            if self._variables != other._variables: return False
-            return True
+        if self.message is not None:
+            return self.message.variables
         else:
-            return False
-    
-    def __key(self):
-        return (self._name, self._type, self._message)
-
-    def __hash__(self):
-        return (self.__key())
+            return set()
 
 
+@attr.s(frozen=True)
 class Wait(Statement, Templatable):
     """
     A wait statement. Shows a message until the user presses enter.
@@ -163,32 +100,14 @@ class Wait(Statement, Templatable):
     $wait : "Get a snack"
     """
 
-    def __init__(self, message: Template):
-        self._message = message
-
-    @property
-    def message(self) -> Template:
-        return self._message
+    message: Template = attr.ib()
 
     @property
     def variables(self) -> set:
-        return self._message.variables
-
-    def __eq__ (self, other):
-        if isinstance (other, Wait):
-            if self._message != other._message: return False
-            if self._message.variables != other._message.variables: return False
-            return True
-        else:
-            return False
-
-    def __key(self):
-        return (self._message)
-
-    def __hash__(self):
-        return (self.__key())
+        return self.message.variables
 
 
+@attr.s(frozen=True)
 class VisibleComment(Statement, Templatable):
     """
     Comments that will be printed to the output.
@@ -196,64 +115,27 @@ class VisibleComment(Statement, Templatable):
     # Creating user {{name}}
     """
 
-    def __init__(self, text: Template):
-        self._text = text
-
-    @property
-    def text(self) -> Template:
-        return self._text
+    text: Template = attr.ib()
 
     @property
     def variables(self) -> set:
-        return self._text.variables
-    
-    def __eq__ (self, other):
-        if isinstance (other, VisibleComment):
-            if self._text != other._text: return False
-            if self._text.variables != other._text.variables: return False 
-            return True
-        else: 
-            return False
-
-    def __key(self):
-        return (self._text)
-
-    def __hash__(self):
-        return (self.__key())
+        return self.text.variables
 
 
+@attr.s(frozen=True)
 class Command(Statement, Templatable):
     """
     A MOLGENIS Commander command.
     """
 
-    def __init__(self, command: Template):
-        self._command = command
-
-    @property
-    def command(self) -> Template:
-        return self._command
+    command: Template = attr.ib()
 
     @property
     def variables(self) -> set:
-        return self._command.variables
-    
-    def __eq__ (self, other):
-        if isinstance (other, Command):
-            if self._command != other._command: return False
-            if self._command.variables != other._command.variables: return False
-            return True
-        else:
-            return False
-    
-    def __key(self):
-        return (self._command)
-
-    def __hash__(self):
-        return (self.__key())
-    
+        return self.command.variables
 
 
+@attr.s(frozen=True)
 class InvisibleComment(Statement):
     """
     Comments that won't be processed in any way.
@@ -261,35 +143,12 @@ class InvisibleComment(Statement):
     // this is a comment
     """
 
-    def __init__(self, comment: str):
-        self._comment = comment
+    comment: str = attr.ib()
 
-    @property
-    def comment(self) -> str:
-        return self._comment
 
-    def __eq__ (self, other):
-        if isinstance (other, InvisibleComment):
-            if self._comment != other._comment: return False
-            return True
-        else:
-            return False
-
-    def __key(self):
-        return (self._comment)
-
-    def __hash__(self):
-        return (self.__key())
-
+@attr.s
 class Empty(Statement):
     """
     An empty line.
     """
     pass
-
-    def __eq__ (self, other):
-        if isinstance (other, Empty): 
-            return True
-        else:
-            return False
-
