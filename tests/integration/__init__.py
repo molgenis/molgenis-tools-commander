a8a46978e7654846465c4d5936aab50611b753fb
tommy <tommydeboer4@gmail.com>
2021-04-02 08:40:45 +0200

    fix integration test configuration

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -7,13 +7,23 @@ import sys
 
 from tests.integration import loader_mock
 
-# Replace the config loader so the test config is loaded
-sys.modules['mcmd.config.loader'] = loader_mock
+
+# noinspection PyProtectedMember,PyUnresolvedReferences,PyTypeChecker
+def mock_config():
+    # Replace the config loader so the test config is loaded
+    sys.modules['mcmd.config.loader'] = loader_mock
+    # Remove the possibility for the config to save itself to disk
+    sys.modules['mcmd.config.config']._persist = lambda: None
+
 
 # noinspection PyProtectedMember,PyUnresolvedReferences
-# Remove the possibility for the config to save itself to disk
-sys.modules['mcmd.config.config']._persist = lambda: None
+def mock_store():
+    import mcmd.core.store
+    # Remove the possibility for the store to save itself to disk
+    sys.modules['mcmd.core.store']._persist = lambda: None
+    # Remove the possibility for the store to load itself from disk
+    sys.modules['mcmd.core.store'].load = lambda: None
 
-# noinspection PyProtectedMember,PyUnresolvedReferences
-# Remove the possibility for the store to save itself to disk
-sys.modules['mcmd.core.store']._persist = lambda: None
+
+mock_config()
+mock_store()
