f9acdc0399f89718f79629c0205ae1c74238ea62
MOLGENIS Jenkins <molgenis+ci@gmail.com>
2021-04-08 15:07:53 +0000

    Bump version: 1.10.0 → 1.10.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='molgenis-commander',
-    version='1.10.0',
+    version='1.10.1',
     packages=find_packages(),
     description='A command line interface for Molgenis',
     url='https://github.com/molgenis/molgenis-tools-commander',
